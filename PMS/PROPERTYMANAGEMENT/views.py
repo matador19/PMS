f71367d6966bbf4a4cf82aae03cfb3096dc6d634
matador19 <alexmaina1997@gmail.com>
2022-04-05 12:53:05 +0300

    add models to views

--- PMS/PROPERTYMANAGEMENT/views.py
+++ PMS/PROPERTYMANAGEMENT/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Property, Tenant, HouseAllocation
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    Githuraiproperty=Property.objects.get(propertyName="Githurai Property")
+    Roysambuproperty=Property.objects.get(propertyName="Roysambu Property")
+    GithuraiTenants = HouseAllocation.objects.filter(propertyName=1)
+    RoysambuTenants = HouseAllocation.objects.filter(propertyName=2)
+    print(GithuraiTenants)
+    return render(request,'home.html',{'Githuraiproperty':Githuraiproperty,'Roysambuproperty':Roysambuproperty,'GithuraiTenants':GithuraiTenants,'RoysambuTenants':RoysambuTenants})
+
+def Githurai(request):
+    Githuraiproperty=Property.objects.get(propertyName="Githurai Property")
+    GithuraiTenants=HouseAllocation.objects.filter(propertyName=1)
+
+    PaymentThresholdOne=16500
+    PaymentThresholdTwo=25000
+    PaymentThresholdThree=30000
+
+    
